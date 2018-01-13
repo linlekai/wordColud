cb016f6032cda85bd0ca3d3864e88870cb36ac53
林乐凯 <luke@linlekaideMBP.lan>
2018-01-13 23:33:37 +0800

    electron-vue for wordColud

---
+++ src/renderer/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div id="app">
+     <div class="layout">
+        <Layout>
+            <Header :style="{position: 'fixed',top:0,zIndex:10, width: '100%'}">
+                <Menu mode="horizontal" theme="dark" active-name="1">
+                    <div class="layout-logo"></div>
+                    <div class="layout-nav">
+                        <MenuItem name="1">
+                            <Icon type="ios-settings"></Icon>
+                           设置
+                        </MenuItem>
+                        <MenuItem name="2">
+                            <Icon type="ios-eye"></Icon>
+                            视图
+                        </MenuItem>
+                        <MenuItem name="3">
+                            <Icon type="ios-book"></Icon>
+                           指南
+                        </MenuItem>
+                    </div>
+                </Menu>
+            </Header>
+            <div class="appview">
+              <router-view>
+                <Content :style="{margin: '88px 20px 0', background: '#fff', minHeight: '500px'}">
+                  Content
+                </Content>
+              </router-view>
+            </div>            
+        </Layout>
+    </div>
+   
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'wordcolud'
+  }
+</script>
+
+<style>
+  #app {
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  }
+  .layout{
+      border: 1px solid #d7dde4;
+      background: #f5f7f9;
+      position: relative;
+      border-radius: 4px;
+      overflow: hidden;
+  }
+  .layout-logo{
+      width: 100px;
+      height: 50px;
+      background: #5b6270;
+      border-radius: 3px;
+      float: left;
+      position: relative;
+      top: 8px;
+      left: 5px;
+      background-image: url("./assets/colud-icon.jpeg");
+      background-position: 50%;
+    background-size: 135%;
+  }
+  .layout-nav{
+      width: 420px;
+      margin: 0 auto;
+      margin-right: 20px;
+  }
+  .layout-footer-center{
+      text-align: center;
+  }
+  .appview {
+    margin-top: 5%;
+  }
+  .layout  .ivu-menu-item {
+    color: #999;
+  }
+</style>
---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,102 @@
+<template>
+  <div id="wrapper">
+    <main>
+        <Row  :gutter="18">
+          <Col span="12">
+            <Card dis-hover	>
+              <p slot="title">画布设置</p>
+              <Tooltip content="留空白使用0.65x的宽度" placement="top-start">
+                <Row class-name="page-size-row" :gutter="32" >
+                  <Col span="12">画布宽度:
+                    <InputNumber :max="1000" :min="200"></InputNumber>
+                  </Col>
+                  <Col span="12">画布高度:
+                    <InputNumber :max="500" :min="100"></InputNumber>
+                  </Col>              
+                </Row>
+              </Tooltip>
+              <Tooltip placement="top-start" >
+                <div slot="content" class="tooltip-content">
+                  <span>调整像素密度显示权重</span>
+                  <span>网格和画布像素大小</span>
+                </div>
+                <Row class-name="row-margin">
+                  <Col span="24">像素密度（dppx）:
+                    <InputNumber :max="10" :min="1"></InputNumber>
+                  </Col>
+                </Row>
+              </Tooltip>
+            </Card>
+          </Col>
+          <Col span="12">
+            <Card dis-hover	>
+              <p slot="title">导入图片</p>
+            </Card>
+          </Col>
+       </Row>
+      <!-- <div class="left-side">
+        <span class="title">
+          Welcome to your new project!
+        </span>
+      </div> -->
+
+      <!-- <div class="right-side">
+        <div class="doc">
+          <div class="title">Getting Started</div>
+          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
+        </div>
+        <div class="doc">
+          <div class="title alt">Other Documentation</div>
+          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
+          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
+        </div>
+      </div> -->
+    </main>
+  </div>
+</template>
+
+<script>
+  import SystemInformation from './LandingPage/SystemInformation'
+import { Button, Table } from 'iview';
+  export default {
+    name: 'landing-page',
+    components: { SystemInformation ,
+      Button,
+      Table
+    },
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    }
+  }
+</script>
+
+<style>
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body { font-family: 'Source Sans Pro', sans-serif; }
+
+  #wrapper {
+    background:
+      radial-gradient(
+        ellipse at top left,
+        rgba(255, 255, 255, 1) 40%,
+        rgba(229, 229, 229, .9) 100%
+      );
+    height: 100vh;
+    padding: 30px 60px;
+    width: 100vw;
+  }
+  .row-margin {
+    margin-top:2%;
+  }
+
+
+
+</style>
